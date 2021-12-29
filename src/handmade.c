f6dfce24b9f4e832ffa8de3e5523c8ed1795988a
Octave <octave.crespel@club.fr>
2021-12-29 14:09:01 +0100

    Clear to red

--- src/handmade.c
+++ src/handmade.c
@@ -1,5 +1,8 @@
 #include "handmade.h"
 
 UPDATE_PROGRAM_AND_RENDER(updateProgramAndRender) {
+    memory->gl.ClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    memory->gl.Clear(GL_COLOR_BUFFER_BIT);
+    
     return false;
 }
